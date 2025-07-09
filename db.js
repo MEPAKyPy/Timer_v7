e2d27dd23981085a225da57619e05575d652c6dc
MEPAKyPy <sananjafarovv@mail.ru>
2025-07-09 19:06:20 +0400

    Update db.js

--- db.js
+++ db.js
@@ -1,32 +1,31 @@
+const { MongoClient, ObjectId } = require('mongodb');
 
-const Database = require('better-sqlite3');
-const db = new Database('users.db');
-
-db.prepare(`
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    timeLeft INTEGER NOT NULL,
-    isRunning INTEGER NOT NULL,
-    startedAt INTEGER,
-    lastResetDate TEXT
-)
-`).run();
+const uri = 'mongodb+srv://sananjafarovv:noRQiwnxX562fUXS@cluster0.kyoo4ly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri);
+const db = client.db('timer_db');
+const usersCollection = db.collection('users');
 
 module.exports = {
-    getAll() {
-        return db.prepare('SELECT * FROM users').all();
+    async getAll() {
+        await client.connect();
+        return await usersCollection.find().toArray();
     },
-    add(user) {
-        db.prepare(`INSERT INTO users (id, name, timeLeft, isRunning, startedAt, lastResetDate)
-                    VALUES (?, ?, ?, ?, ?, ?)`)
-          .run(user.id, user.name, user.timeLeft, user.isRunning ? 1 : 0, user.startedAt, user.lastResetDate);
+
+    async add(user) {
+        await client.connect();
+        await usersCollection.insertOne(user);
     },
-    update(user) {
-        db.prepare(`UPDATE users SET timeLeft = ?, isRunning = ?, startedAt = ?, lastResetDate = ? WHERE id = ?`)
-          .run(user.timeLeft, user.isRunning ? 1 : 0, user.startedAt, user.lastResetDate, user.id);
+
+    async update(user) {
+        await client.connect();
+        await usersCollection.updateOne(
+            { id: user.id },
+            { $set: user }
+        );
     },
-    remove(id) {
-        db.prepare('DELETE FROM users WHERE id = ?').run(id);
+
+    async remove(id) {
+        await client.connect();
+        await usersCollection.deleteOne({ id });
     }
 };
