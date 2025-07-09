c1a987d270d886f62533ff70128d9760a2e1c476
MEPAKyPy <sananjafarovv@mail.ru>
2025-07-09 20:25:20 +0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 
+const FILE = path.join(__dirname, 'users.json');
+
+function readUsers() {
+    try {
+        const data = fs.readFileSync(FILE, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+function writeUsers(users) {
+    fs.writeFileSync(FILE, JSON.stringify(users, null, 2));
+}
+
+module.exports = { readUsers, writeUsers };
